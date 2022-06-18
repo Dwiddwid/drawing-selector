1796fa3f5fdef13f4148c898d30ab90054dc830e
David Bretz <Dwiddwid@users.noreply.github.com>
2022-06-17 19:10:34 -0800

    Changed file names and moved things around
    Added a spy logo to the home page since the first outing of this program will be at a spy themed event. This page is planned to become the admin page. When navigating to the Drawing page, navigation elements are hidden to not distract from the drawing itself.

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,12 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import { ref } from 'vue'
 import HelloWorld from '@/components/HelloWorld.vue'
+
+const drawingStarted = ref(false); 
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
   <RouterView />
 </template>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,47 @@ import TheWelcome from '@/components/TheWelcome.vue'
 </script>
 
 <template>
+<header>
+    <img alt="Spy" class="logo" src="@/assets/user-spy.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/drawing">Start drawing</RouterLink>
+      </nav>
+    </div>
+  </header>
   <main>
     <TheWelcome />
   </main>
 </template>
+
+<style>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+</style>
