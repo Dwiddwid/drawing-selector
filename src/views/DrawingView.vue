50412bd5400f5b09c62cb3c7ab97843fd0b94f8b
David Bretz <Dwiddwid@users.noreply.github.com>
2022-06-20 15:21:05 -0800

    Patched error
    The drawing page was looking at the property of a null object.

--- src/views/DrawingView.vue
+++ src/views/DrawingView.vue
@@ -10,10 +10,12 @@ const store = useParticipantStore();
     <div class="d-flex-column align-items-center">
       <h1 v-if="store.index < 0">Ready to start drawing!</h1>
       <h1 v-else-if="store.winnerSelected">Winner!</h1>
-      <h2 v-if="store.index > -1">
-        {{store.candidates[store.index]['First Name']}} {{store.currentCandidate['Last Name']}}
-      </h2>
-      <div class="mb-4">{{store.currentCandidate['School Grade']}}</div>
+      <div v-if="store.index > -1">
+        <h2>
+          {{store.candidates[store.index]['First Name']}} {{store.currentCandidate['Last Name']}}
+        </h2>
+        <div class="mb-4">{{store.currentCandidate['School Grade']}}</div>
+      </div>
       <button type="button" class="btn btn-primary mt-4" v-if="!store.spinning" v-on:click="store.selectRandomCandidate()">GO!</button>
     </div>
   </div>
