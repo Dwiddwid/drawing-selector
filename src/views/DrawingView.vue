5ec2f59002a64bcbe73c8ae77cdf4d7a8d830dbc
David Bretz <Dwiddwid@users.noreply.github.com>
2022-06-20 15:54:19 -0800

    Updated title and scaled drawing font
    Font size for drawing scales with screen size.

--- src/views/DrawingView.vue
+++ src/views/DrawingView.vue
@@ -6,17 +6,21 @@ const store = useParticipantStore();
 </script>
 
 <template>
-  <div tabindex="0" class="name-display d-flex">
-    <div class="d-flex-column align-items-center">
-      <h1 v-if="store.index < 0">Ready to start drawing!</h1>
-      <h1 v-else-if="store.winnerSelected">Winner!</h1>
-      <div v-if="store.index > -1">
-        <h2>
-          {{store.candidates[store.index]['First Name']}} {{store.currentCandidate['Last Name']}}
-        </h2>
-        <div class="mb-4">{{store.currentCandidate['School Grade']}}</div>
+  <div class="container-fluid">
+    <div tabindex="0" class="row name-display d-flex">
+      <div class="col">
+        <div class="scaled-text">
+          <h1 v-if="store.index < 0">Ready to start drawing!</h1>
+          <h1 v-else-if="store.winnerSelected">Winner!</h1>
+          <div v-if="store.index > -1">
+            <h2>
+              {{store.candidates[store.index]['First Name']}} {{store.currentCandidate['Last Name']}}
+            </h2>
+            <div class="mb-4">{{store.currentCandidate['School Grade']}}</div>
+          </div>
+        </div>
+        <button type="button" class="btn btn-primary mt-4" v-if="!store.spinning" v-on:click="store.selectRandomCandidate()">GO!</button>
       </div>
-      <button type="button" class="btn btn-primary mt-4" v-if="!store.spinning" v-on:click="store.selectRandomCandidate()">GO!</button>
     </div>
   </div>
 </template>
@@ -26,6 +30,9 @@ button {
   width: 100%;
 }
 @media (min-width: 1024px) {
+  .scaled-text * {
+    font-size: 4vw;
+  }
   .name-display {
     min-height: 100vh;
     min-width:100vw;
